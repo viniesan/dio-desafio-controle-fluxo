08a68a8d59f14545aaab6dd4bb1bdeb140eb61a2
Viniesan <santanahazzo@gmail.com>
2024-07-24 23:38:41 -0300

    Criando o projeto e adiconado os arquivos indesejados ao gitignore

---
+++ src/DesafioContador/ParametrosInvalidosException.java
@@ -0,0 +1,8 @@
+package DesafioContador;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
+
